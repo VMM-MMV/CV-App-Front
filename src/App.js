3f40b16557ba9de56980a3d3535c565c2cb429d5
VMM-MMV <121231490+VMM-MMV@users.noreply.github.com>
2023-12-04 14:30:04 +0200

    changed backend end node

--- src/App.js
+++ src/App.js
@@ -63,7 +63,7 @@ function App() {
     try {
       console.log(collectedData);
       // setFormSubmitted(true);
-      await axios.post('http://ec2-34-224-91-231.compute-1.amazonaws.com:80/addPerson', {
+      await axios.post('https://34.224.91.231/addPerson', {
         name: collectedData.person.name,
         lastname: collectedData.person.lastname,
         address: collectedData.person.address,
